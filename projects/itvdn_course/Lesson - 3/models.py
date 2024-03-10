592f755a694d66f6c96895a7788fc7de4a5119a6
IvanSlivenko <ivan8822@ukr.net>
2024-03-10 11:22:50 +0300

    10-03-2024-11-22

---
+++ projects/itvdn_course/Lesson - 3/models.py
@@ -0,0 +1,18 @@
+from peewee import (SqliteDatabase, Model, IntegerField, DoubleField,
+                    DateTimeField, datetime as peewee_datetime)
+import sqlite3
+
+db = SqliteDatabase("golden-eye.db")# підключаємо базу данних
+
+class XRate(Model):
+    class Meta:
+        database = db # назва бази данних
+        db_table = "xrates" # назва таблиці
+        indexes = (
+            (("from_currensy", "to_currency"),True),
+        )# індекси та ознака унікальності = True
+
+from_currency = IntegerField()
+to_currency = IntegerField()
+rate = DoubleField()
+updated = DateTimeField(default=peewee_datetime.datetime.now)
